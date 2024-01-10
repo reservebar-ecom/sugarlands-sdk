82648759fc0e6a7f30307df44115248b2963555e
Lucas Borges <lucaspdfborges@gmail.com>
2024-01-10 16:32:11 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,130 @@
+const products = [
+    {
+        "name": "Eggo Brunch In A Jar Waffles & Syrup Cream Liqueur",
+        "id": "GROUPING-2215790"
+    },
+    {
+        "name": "Sugarlands Appalachian Apple Pie Moonshine",
+        "id": "GROUPING-324288"
+    },
+    {
+        "name": "Sugarlands Appalachian Peanut Butter Sippin' Cream Liqueur",
+        "id": "GROUPING-2187236"
+    },
+    {
+        "name": "Sugarlands Banana Pudding Appalachian Sippin' Cream",
+        "id": "GROUPING-1442835"
+    },
+    {
+        "name": "Sugarlands Blockader's Blackberry Moonshine",
+        "id": "GROUPING-408544"
+    },
+    {
+        "name": "Sugarlands Blueberry Muffin Moonshine",
+        "id": "GROUPING-1368266"
+    },
+    {
+        "name": "Sugarlands Bristol Motor Speedway 60th Anniversary Corn Whiskey",
+        "id": "GROUPING-1771977"
+    },
+    {
+        "name": "Sugarlands Butter Pecan Appalachian Sippin' Cream",
+        "id": "GROUPING-1720875"
+    },
+    {
+        "name": "Sugarlands Butterscotch Gold Moonshine",
+        "id": "GROUPING-1437798"
+    },
+    {
+        "name": "Sugarlands Chipper Jones' Sweet Tea Moonshine",
+        "id": "GROUPING-1907258"
+    },
+    {
+        "name": "Sugarlands Cole Swindell's Peppermint Moonshine",
+        "id": "GROUPING-408543"
+    },
+    {
+        "name": "Sugarlands Cole Swindell's Pre Show Punch",
+        "id": "GROUPING-1324766"
+    },
+    {
+        "name": "Sugarlands Dark Chocolate Coffee Appalachian Sippin' Cream",
+        "id": "GROUPING-597128"
+    },
+    {
+        "name": "Sugarlands Eggnog Appalachian Sippin' Cream",
+        "id": "GROUPING-1017522"
+    },
+    {
+        "name": "Sugarlands Electric Orange Appalachian Sippin' Cream",
+        "id": "GROUPING-892758"
+    },
+    {
+        "name": "Sugarlands Jim Tom Hendrick's Signature Moonshine",
+        "id": "GROUPING-593357"
+    },
+    {
+        "name": "Sugarlands Maple Bacon Moonshine",
+        "id": "GROUPING-1368287"
+    },
+    {
+        "name": "Sugarlands Mark & Digger's Hazelnut Rum",
+        "id": "GROUPING-612276"
+    },
+    {
+        "name": "Sugarlands Mark & Digger's Rye Apple Moonshine",
+        "id": "GROUPING-1417007"
+    },
+    {
+        "name": "Sugarlands Mark Rogers' American Peach Moonshine",
+        "id": "GROUPING-324287"
+    },
+    {
+        "name": "Sugarlands Moonshine Collection",
+        "id": "GROUPING-BUNDLE-100000581"
+    },
+    {
+        "name": "Sugarlands Moonshine Collection",
+        "id": "GROUPING-1771508"
+    },
+    {
+        "name": "Sugarlands Peanut Butter & Jelly Moonshine",
+        "id": "GROUPING-1133836"
+    },
+    {
+        "name": "Sugarlands Piña Colada Moonshine",
+        "id": "GROUPING-609997"
+    },
+    {
+        "name": "Sugarlands Pumpkin Spice Latte Appalachian Sippin' Cream",
+        "id": "GROUPING-1368286"
+    },
+    {
+        "name": "Sugarlands Root Beer Moonshine",
+        "id": "GROUPING-1080880"
+    },
+    {
+        "name": "Sugarlands Ryder Cup Lemonade Moonshine",
+        "id": "GROUPING-1832980"
+    },
+    {
+        "name": "Sugarlands Silver Cloud Tennessee Sour Mash Moonshine",
+        "id": "GROUPING-469863"
+    },
+    {
+        "name": "Sugarlands Southern Sweet Tea Moonshine",
+        "id": "GROUPING-324286"
+    },
+    {
+        "name": "Sugarlands Strawberry Dream Appalachian Sippin' Cream",
+        "id": "GROUPING-1017541"
+    },
+    {
+        "name": "Sugarlands Tickle's Dynamite Cinnamon Moonshine",
+        "id": "GROUPING-408545"
+    },
+    {
+        "name": "High Rock Vodka",
+        "id": "GROUPING-1824475"
+    }
+];
